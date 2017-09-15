8eb7d82bdf09810c9d975b80f5aea02f7b9b8e2b
Franky Liang <fliangz96@gmail.com>
2017-09-15 00:54:10 -0400

    FACEBOOK: add better recursive solution for min depth of binary tree

--- fb/easy/111-min-depth-of-binary-tree.py
+++ fb/easy/111-min-depth-of-binary-tree.py
@@ -39,8 +39,7 @@ class Solution(object):
         """
         if not root:
             return 0
-        if not root.left:
-            return 1 + self.minDepth(root.right)
-        if not root.right:
-            return 1 + self.minDepth(root.right)
+        left_depth = self.minDepth(root.left)
+        right_depth = self.minDepth(root.right)
+        
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
