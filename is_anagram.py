909377b10ac990dfed330178f5b9b42db4a5a06c
Franky Liang <fliangz96@gmail.com>
2017-09-19 13:01:30 -0400

    MSFT: is anagram using hash map

---
+++ is_anagram.py
@@ -0,0 +1,22 @@
+a = 'a bc'
+b = 'casdab'
+
+char_counter = {}
+for char in a:
+    if char.isspace():
+        continue
+    if char in char_counter:
+        char_counter[char] += 1
+    else:
+        char_counter[char] = 1
+
+for char in b:
+    if char not in char_counter:
+        print("Not anagram!")
+    if char in char_counter:
+        char_counter[char] -= 1
+        if char_counter[char] == 0:
+            del char_counter[char]
+
+if not char_counter:
+    print("Anagram!")
