1552f8034a5bebd6c1307c05e4a826915268ee7a
Franky Liang <fliangz96@gmail.com>
2017-09-20 20:53:20 -0400

    FB: add solution for 311-sparse-matrix-mult

---
+++ fb/leetcodelocked/medium/311-sparse-matrix-multiplication.py
@@ -0,0 +1,59 @@
+"""
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+"""
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # Naive solution is to multiple and return
+        # How to optimize multiplication?
+        res = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(B[0])):
+                for k in range(len(B)):
+                    res[i][j] += A[i][k] * B[j][k]
+
+        return res
+        
+class Solution(object):
+        # Optimization!
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # Naive solution is to multiple and return
+        # How to optimize multiplication?
+        res = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if not A[i][j] == 0:
+                    for k in range(len(B[0])):
+                        if not B[j][k] == 0:
+                            res[i][k] += A[i][j] * B[j][k]
+
+        return res
