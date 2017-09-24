e3ebd586d3a86f9d384175fcf2f23016759118d5
Franky Liang <fliangz96@gmail.com>
2017-09-23 23:47:55 -0400

    FB: add new solution for add binary
    add binary like normally, use %2 and /2 on a carry, concatenate to result string

--- fb/easy/67-add-binary.py
+++ fb/easy/67-add-binary.py
@@ -29,7 +29,7 @@ class Solution(object):
                 bin_str = '0' + bin_str
             num /= 2
         return bin_str
-        
+
     def addBinary(self, a, b):
         """
         :type a: str
@@ -38,3 +38,33 @@ class Solution(object):
         """
         add_sum = self.convert_to_number(a) + self.convert_to_number(b)
         return self.convert_to_binary(add_sum)
+
+
+"""
+Pretty much just add like you normally would for binary
+Think when to %2 and when to /2
+"""
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        res = ''
+        while (i >= 0) or (j >= 0):
+            s = carry
+            if i >= 0:
+                s += int(a[i])
+                i -= 1
+            if j >= 0:
+                s += int(b[j])
+                j -= 1
+            res = str(s % 2) + res
+            carry = s / 2
+        if carry:
+            res = str(carry) + res
+        return res
